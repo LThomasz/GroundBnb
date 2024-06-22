fac4eaf47e03569fb8bac11c31c35cf763a25fba
Lyndon Thomasz <137991508+LThomasz@users.noreply.github.com>
2024-06-22 15:22:00 -0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1>Next Starter</h1>
+      </div>
+    </section>
+
+  )
+}
