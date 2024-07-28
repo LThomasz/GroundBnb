2e496671d0c82257422ea4a6c24e359edbdae58f
Lyndon <tzlyndon@gmail.com>
2024-07-27 21:24:06 -0700

    used Card component to make customizable Card with title description and body

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,29 @@
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function AdminDashboard() {
   return (
     <div>
-      <Card></Card>
+      test
     </div>
+  )
+}
+
+type SpotCardProps = {
+  title: string
+  description: string
+  body: string
+}
+
+function SpotCard({title, description, body}: SpotCardProps) {
+  return (
+    <Card>
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{description}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>{body}</p>
+        </CardContent>
+      </Card>
   )
 }
